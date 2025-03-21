1500dbc1c1d21fe918889d7d4bea1ca9c582c4d2
Savio Qalby <152057506+mqsavio21@users.noreply.github.com>
2025-03-21 13:06:56 +0700

    123

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [vue()],
+    base: '/',
     resolve: {
         alias: {
             '@': resolve(__dirname, './resources'),
@@ -13,9 +14,23 @@ export default defineConfig({
     build: {
         outDir: 'dist',
         emptyOutDir: true,
+        assetsDir: 'assets',
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html')
+            },
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.')[1];
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'images';
+                    } else if (/woff|woff2/i.test(extType)) {
+                        extType = 'fonts';
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js'
             }
         }
     }
