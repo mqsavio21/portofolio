1d6b1bc75d1261877610405fc41b8c622ef3ea5d
Savio Qalby <152057506+mqsavio21@users.noreply.github.com>
2025-03-21 10:52:46 +0700

    change

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            publicDirectory: 'public',
         }),
         vue({
             template: {
@@ -19,11 +21,17 @@ export default defineConfig({
     ],
     build: {
         outDir: 'dist',
+        assetsDir: 'assets',
+        emptyOutDir: true,
         manifest: true,
         rollupOptions: {
-            output: {
-                manualChunks: undefined
-            }
+            input: 'resources/js/app.js'
+        }
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './resources'),
+            '~': resolve(__dirname, './public')
         }
     }
 });
